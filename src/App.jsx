88a2172c09d464c031e96092ac3ca984ba6ed91a
Tajudeen khabab Akanbi <152615493+TajjDev@users.noreply.github.com>
2025-10-23 13:29:18 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
     <>
     <ProfileII />
     {/* <UserProfile/> */}
-    <UserCreation/>
+    
     {/* <BrowserRouter> */}
       {/* <Routes> */}
         {/* <Route path='/' element={""}/> */}
