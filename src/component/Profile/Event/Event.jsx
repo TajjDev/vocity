17f91da77bfb2ccb177fe57013f9fbcf25627fca
Tajudeen khabab Akanbi <152615493+TajjDev@users.noreply.github.com>
2025-11-16 13:06:11 +0100

    Update Event.jsx

--- src/component/Profile/Event/Event.jsx
+++ src/component/Profile/Event/Event.jsx
@@ -479,6 +479,54 @@ const Event = ({ postId }) => {
                                     <p style={{ paddingTop: "5px", textTransform: "capitalize", fontSize: "0.9rem" }}> </p>
                                 </div>
                             </div>
+                            <div style={{ gap: "15px", borderRadius: "10px", border: "1px solid #ffffff22", background: "#0000003d", marginBottom: "10px", display: "flex", flexDirection: "row", justifyContent: "start", textAlign: "left", padding: "10px 20px" }}>
+                                <div style={{ display: "flex" }}>
+                                    <img className="fitt" src="" /*alt={c.title}*/ style={{ width: "40px", height: "40px", opacity: "0.5", borderRadius: "100px", background: "#fff", marginTop: "5px" }} />
+                                </div>
+                                <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+                                    {/* <p style={{ display: "flex", flexDirection: "column" }}> */}
+                                    <p style={{ color: "#fff", fontSize: "0.9rem", height: "10px", width: "150px", background: "#fff", opacity: "0.3", borderRadius: "10px" }}>
+
+                                    </p>
+                                    <p style={{ color: "#fff", fontSize: "0.9rem", height: "40px", width: "200px", background: "#fff", opacity: "0.3", borderRadius: "10px" }}>
+                                        {/* date */}
+                                    </p>
+                                    {/* </p> */}
+                                    <p style={{ paddingTop: "5px", textTransform: "capitalize", fontSize: "0.9rem" }}> </p>
+                                </div>
+                            </div>
+                            <div style={{ gap: "15px", borderRadius: "10px", border: "1px solid #ffffff22", background: "#0000003d", marginBottom: "10px", display: "flex", flexDirection: "row", justifyContent: "start", textAlign: "left", padding: "10px 20px" }}>
+                                <div style={{ display: "flex" }}>
+                                    <img className="fitt" src="" /*alt={c.title}*/ style={{ width: "40px", height: "40px", opacity: "0.5", borderRadius: "100px", background: "#fff", marginTop: "5px" }} />
+                                </div>
+                                <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+                                    {/* <p style={{ display: "flex", flexDirection: "column" }}> */}
+                                    <p style={{ color: "#fff", fontSize: "0.9rem", height: "10px", width: "150px", background: "#fff", opacity: "0.3", borderRadius: "10px" }}>
+
+                                    </p>
+                                    <p style={{ color: "#fff", fontSize: "0.9rem", height: "40px", width: "200px", background: "#fff", opacity: "0.3", borderRadius: "10px" }}>
+                                        {/* date */}
+                                    </p>
+                                    {/* </p> */}
+                                    <p style={{ paddingTop: "5px", textTransform: "capitalize", fontSize: "0.9rem" }}> </p>
+                                </div>
+                            </div>
+                            <div style={{ gap: "15px", borderRadius: "10px", border: "1px solid #ffffff22", background: "#0000003d", marginBottom: "10px", display: "flex", flexDirection: "row", justifyContent: "start", textAlign: "left", padding: "10px 20px" }}>
+                                <div style={{ display: "flex" }}>
+                                    <img className="fitt" src="" /*alt={c.title}*/ style={{ width: "40px", height: "40px", opacity: "0.5", borderRadius: "100px", background: "#fff", marginTop: "5px" }} />
+                                </div>
+                                <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+                                    {/* <p style={{ display: "flex", flexDirection: "column" }}> */}
+                                    <p style={{ color: "#fff", fontSize: "0.9rem", height: "10px", width: "150px", background: "#fff", opacity: "0.3", borderRadius: "10px" }}>
+
+                                    </p>
+                                    <p style={{ color: "#fff", fontSize: "0.9rem", height: "40px", width: "200px", background: "#fff", opacity: "0.3", borderRadius: "10px" }}>
+                                        {/* date */}
+                                    </p>
+                                    {/* </p> */}
+                                    <p style={{ paddingTop: "5px", textTransform: "capitalize", fontSize: "0.9rem" }}> </p>
+                                </div>
+                            </div>
                         </>
                     ) : activeSubTab === "comments" ? (
                         subTabData.length === 0 || subTabData.every(c => !c.comment) ? (
@@ -661,6 +709,7 @@ const Event = ({ postId }) => {
 };
 
 export default Event;
+
 
 
 
